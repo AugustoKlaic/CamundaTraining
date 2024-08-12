cc948bd02db03900ca35bc7710a4db9de813cdf3
Augusto Sopelsa Klaic <ntconsult.augusto@agi.com.br>
2024-08-12 11:53:38 -0300

    added embedded forms to process

--- src/main/java/org/bpmn/training/camundatraining/CamundaTrainingApplication.java
+++ src/main/java/org/bpmn/training/camundatraining/CamundaTrainingApplication.java
@@ -1,9 +1,11 @@
 package org.bpmn.training.camundatraining;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class CamundaTrainingApplication {
 
 	public static void main(String[] args) {
